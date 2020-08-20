f2ed0858039f18f556b8c3d8fc732ff9ff6484c3
Josh Wolfe <jfwolfe@spscommerce.com>
2020-08-20 17:13:06 -0400

    Restructured project to combine two commands for simpler usage

--- plate.py
+++ plate.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import os
+import re
+import subprocess
 from pathlib import Path
 
 import fire
@@ -18,6 +20,7 @@ class CliFunctions(object):
         self.__config = {}
         self.__env = Environment(undefined=DebugUndefined)
         self.__env.undefined = StrictUndefined
+        self.__file_extension_pattern = re.compile('.plate(.yml|.yml)?$')
 
         file_name = os.path.join(
             os.environ['HOME'],
@@ -30,16 +33,28 @@ class CliFunctions(object):
         with open(file_name) as f:
             self.__config = yaml.safe_load(f.read())
 
+    def deploy(self):
+        # ['kubectl', 'apply', '--all', '--prune', '-f', '-']
+        process = subprocess.Popen(['kubectl'], stdout=subprocess.PIPE)
 
-    def render(self, *in_files, file_glob='*', output_dir=None):
+        output = process.stdout.readline()
+        while process.poll() is None or output != b'':
+            print(output)
+            output = process.stdout.readline()
+
+    def build(self, *in_files, file_glob='*', output_dir=None, template_dir=None):
         has_std_data = not sys.stdin.isatty()
 
         if not has_std_data and len(in_files) <= 0:
             raise CliErrorException('You must specify at least one file or stdinput')
 
+        if template_dir is not None:
+            self.__env.loader = FileSystemLoader(template_dir)
+
         file_data_list = []
         for path in in_files:
-            file_data_list.extend(self.__create_path_list_and_generate_file_data_models(path, file_glob))
+            models = self.__create_path_list_and_generate_file_data_models(path, file_glob, template_dir)
+            file_data_list.extend(models)
 
         if has_std_data:
             user_input = sys.stdin.read()
@@ -49,54 +64,63 @@ class CliFunctions(object):
         return Render(self.__config, self.__env, file_data_list)
 
 
-    def generate(self, in_file, template_dir, file_glob='*'):
-        file_list = self.__create_path_list(in_file, file_glob)
-        self.__env.loader = FileSystemLoader(template_dir)
+    def __process_user_template(self, file_content, file_path, template_dir):
+        body = yaml.safe_load(file_content)
 
-        for file_path in file_list:
-            with open(file_path) as f:
-                body = yaml.safe_load(f)
+        if 'template' not in body:
+            print(f'{file_path}: Missing template name', file=sys.stderr)
+            return
+        if 'vars' not in body:
+            print(f'{file_path}: Missing variables', file=sys.stderr)
+            return
 
-                if 'template' not in body:
-                    print('Missing template name', file=sys.stderr)
-                    return
-                if 'vars' not in body:
-                    print('Missing variables', file=sys.stderr)
-                    return
+        templates = body['template'] if isinstance(body['template'], list) else [body['template']]
 
-                templates = body['template'] if isinstance(body['template'], list) else [body['template']]
-                for template in templates:
-                    self.__render_and_output_template(template, body['vars'])
+        user_template_results = []
+        for template in templates:
+            if template_dir is None:
+                target = Path(os.path.join(file_path.parent, template))
+                self.__env.loader = FileSystemLoader(target.parent)
 
+            user_template_results.append(self.__render_and_output_template(
+                os.path.basename(template),
+                body['vars']
+            ))
+
+        return '\n---\n'.join(user_template_results)
 
     def __render_and_output_template(self, template, variables):
         template = self.__env.get_template(template)
-        rendered = template.render(variables)
-        print(rendered)
-        print('---')
+        return template.render(variables)
 
 
     def __create_path_list(self, in_file, file_glob):
         return [Path(in_file)] if os.path.isfile(in_file) else list(Path(in_file).rglob(file_glob))
 
 
-    def __create_path_list_and_generate_file_data_models(self, in_file, file_glob):
+    def __create_path_list_and_generate_file_data_models(self, in_file, file_glob, template_dir):
         paths = self.__create_path_list(in_file, file_glob)
 
-        return list(self.__generate_file_data_models(paths))
+        return list(self.__generate_file_data_models(paths, template_dir))
 
     def __scan_str_for_variables(self, source):
         ast = self.__env.parse(source)
         return find_undeclared_variables(ast)
 
-    def __generate_file_data_models(self, paths):
+    def __generate_file_data_models(self, paths, template_dir):
         for path in paths:
-            if not os.path.isdir(path):
-                with open(path, 'r') as src_file:
-                    source = src_file.read()
-                    undefined = self.__scan_str_for_variables(source)
+            if os.path.isdir(path):
+                return
 
-                    yield FileData(source, undefined, path.as_posix())
+            with open(path, 'r') as src_file:
+                source = src_file.read()
+
+                if self.__file_extension_pattern.search(path.as_posix()):
+                    source = self.__process_user_template(source, path, template_dir)
+
+                undefined = self.__scan_str_for_variables(source)
+
+                yield FileData(source, undefined, path.as_posix())
 
 
 if __name__ == '__main__':
